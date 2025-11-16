cd057648e70c0906a584a914a339ae0bb83ae0b1
Meetzli <63364127+meetzli@users.noreply.github.com>
2025-11-16 19:40:11 +0100

    bump versions and update example

--- examples/homepage.rs
+++ examples/homepage.rs
@@ -1,15 +1,16 @@
 use dioxus::prelude::*;
 use dioxus_html_macro::html;
 
-fn app(cx: Scope) -> Element {
-    let mut count = use_state(cx, || 0);
-    cx.render(html!(
+fn app() -> Element {
+    let mut count = use_signal(||0);
+
+    html!(
         <h1>"High-Five counter: {count}"</h1>
         <button onclick={move |_| count += 1}>"Up high!"</button>
         <button onclick={move |_| count -= 1}>"Down low!"</button>
-    ))
+    )
 }
 
 fn main() {
-    dioxus_desktop::launch(app);
+    launch(app);
 }
